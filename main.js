1bf5b8b94a6952272a9fb32e26b84bfde8c9010c
Oryam <48219965+or-yam@users.noreply.github.com>
2024-10-02 15:51:56 +0300

    Add next and prev buttons

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
-const searchBtn = document.getElementById('search-btn'); // search button
-const inputField = document.getElementById('name-input'); // search field input
-const nameScreen = document.getElementById('name-screen'); //name-screen
-const imageScreen = document.getElementById('main-screen'); // image screen
-const aboutScreen = document.getElementById('about-screen'); // about-text screen
-const typeScreen = document.getElementById('type-screen'); // type screen
-const idScreen = document.getElementById('id-screen'); // spices screen
+const searchBtn = document.getElementById('search-btn');
+const inputField = document.getElementById('name-input');
+const nameScreen = document.getElementById('name-screen');
+const imageScreen = document.getElementById('main-screen');
+const aboutScreen = document.getElementById('about-screen');
+const typeScreen = document.getElementById('type-screen');
+const idScreen = document.getElementById('id-screen');
+const rightButton = document.querySelector('.right-nav-button');
+const leftButton = document.querySelector('.left-nav-button');
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+let current = 1;
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-};
+      .register('/serviceWorker.js')
+      .then((res) => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err));
+  });
+}
 
 const getPokemonData = (pokemon) => {
+  if (!pokemon) {
+    return;
+  }
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      if(!data.id){
-        return
+      if (!data.id) {
+        return;
       }
+      current = data.id;
       let id = ('00' + data.id).slice(-3);
       imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
       nameScreen.innerHTML = data.name;
       typeScreen.innerHTML = data.types[0].type.name;
       idScreen.innerHTML = `#${data.id}`;
-      aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${
-        data.weight / 10
-      }kg`;
+      aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${data.weight / 10}kg`;
       inputField.value = '';
     });
 };
 
-inputField.addEventListener(
-  'keydown',
-  (event) => event.key === 'Enter' && searchBtn.click()
-);
+inputField.addEventListener('keydown', (event) => event.key === 'Enter' && searchBtn.click());
 searchBtn.addEventListener('click', () => getPokemonData(inputField.value));
+rightButton.addEventListener('click', () => {
+  getPokemonData(current + 1);
+});
+leftButton.addEventListener('click', () => {
+  if (current <= 1) {
+    return;
+  }
+  getPokemonData(current - 1);
+});
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowRight') {
+    rightButton.click();
+  } else if (event.key === 'ArrowLeft') {
+    leftButton.click();
+  }
+});
